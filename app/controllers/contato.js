8b1a3775741ceebf1386fa672ed55e7405282067
jonnatas <jonatas_lenon@hotmail.com>
2017-06-06 19:34:52 -0300

    comercial

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -73,8 +73,7 @@ module.exports.mail = function(application, req, res) {
 
     var mailOptions = {
         from: req.body.nome + ' <site@jofen.com.br>',
-        to: 'site@jofen.com.br',
-        //        to: 'site@jofen.com.br, comercial2@jofen.com.br',
+        to: 'site@jofen.com.br, comercial2@jofen.com.br',
         subject: 'Contato site jofen: ' + req.body.assunto,
         html: '<H1>Email: ' + req.body.email + '</H1>Nome: ' + '<h2>' + req.body.nome + ' </H2> <h3> Telefone: ' + req.body.telefone + '</h3><p>Mensagem: ' + req.body.mensagem + '</p>'
     };
