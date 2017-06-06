9c3b8d6e63c4dabc3219f53b855734fa67266034
jonnatas <jonatas_lenon@hotmail.com>
2017-06-06 10:25:27 -0300

    testando envio com o hotmail

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -70,7 +70,7 @@ module.exports.mail = function(application, req, res) {
     var mailOptions = {
         from: 'site' + ' <site@jofen.com.br>',
         //to: 'jonatastestelennon@gmail.com, comercial2@jofen.com.br',
-        to: 'site@jofen.com.br, jonatastestelennon@gmail.com, jonatas_lenon@hotmail.com',
+        to: 'comercial2@jofen.com.br, site@jofen.com.br, jonatastestelennon@gmail.com, jonatas_lenon@hotmail.com',
         subject: 'Contato site jofen: ' + req.body.assunto,
         html: '<H1>Email: ' + req.body.email + '</H1>Nome: ' + '<h2>' + req.body.nome + ' </H2> <h3> Telefone: ' + req.body.telefone + '</h3><p>Mensagem: ' + req.body.mensagem + '</p>'
     };
